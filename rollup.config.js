cbc1e16195639b424d2c6f3ed90d48f19a505aac
Shivam Mishra <scm.mymail@gmail.com>
2020-09-18 12:02:45 +0530

    feat: add data to testbed

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import sveltePreprocess from 'svelte-preprocess'
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
 	let server;
-	
+
 	function toExit() {
 		if (server) server.kill(0);
 	}
@@ -43,7 +44,8 @@ export default {
 			// a separate file - better for performance
 			css: css => {
 				css.write('public/build/bundle.css');
-			}
+			},
+			preprocess: sveltePreprocess(),
 		}),
 
 		// If you have external dependencies installed from
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,36 @@
 <script>
-	export let name;
+  import { onMount } from "svelte";
+  import arcticData from "./data.js";
+  import { Chart } from "frappe-charts/dist/frappe-charts.esm.js";
+
+  let config = {
+    data: arcticData,
+    title: "Arctic sea ice minimum",
+    type: "bar", // or 'bar', 'line', 'pie', 'percentage'
+    height: 300,
+    tooltipOptions: {
+      formatTooltipX: d => (d + "").toUpperCase(),
+      formatTooltipY: d => d + " million sq km"
+    },
+    lineOptions: {
+      regionFill: 1
+    },
+    axisOptions: {
+      xIsSeries: true,
+      xAxisMode: "tick"
+    }
+    // colors: ["blue"]
+  };
+  let chartRef;
+  onMount(() => {
+    let chart = new Chart(chartRef, config);
+  });
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+<style global>
+  @import "../node_modules/frappe-charts/dist/frappe-charts.min.css";
 </style>
+
+<main>
+  <div bind:this={chartRef} />
+</main>
